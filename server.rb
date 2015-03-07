dd31d93d835a590b1e676e971eebaddb9bc59c39
kyjoya <kyleejacker@gmail.com>
2015-03-07 16:09:56 -0500

    fixes server file to include database logic

--- server.rb
+++ server.rb
@@ -3,22 +3,75 @@ require 'pg'
 require 'pry'
 load 'helpers.rb'
 
-get '/actors' do
+def movies
+  db_connection do |conn|
+  conn.exec('SELECT movies.id, movies.title, movies.year, movies.rating, genres.name AS genre, studios.name AS studio FROM movies
+      JOIN genres ON movies.genre_id = genres.id
+      LEFT OUTER JOIN studios ON movies.studio_id = studios.id
+      ORDER BY movies.title ASC')
+    end
+end
 
+def movie_data
+movie_id = params[:id]
+
+db_connection do |conn|
+conn.exec("SELECT movies.id, movies.title, movies.year, movies.rating, genres.name AS genre, studios.name AS studio FROM movies
+    JOIN genres ON movies.genre_id = genres.id
+    LEFT OUTER JOIN studios ON movies.studio_id = studios.id
+    WHERE movies.id = #{movie_id}")
+  end
+end
+
+def cast_data
+movie_id = params[:id]
+
+db_connection do |conn|
+conn.exec("SELECT actors.id, actors.name, cast_members.character FROM cast_members
+      JOIN actors ON cast_members.actor_id = actors.id
+      WHERE cast_members.movie_id = #{movie_id}")
+  end
+end
+
+def actors
+db_connection do |conn|
+conn.exec("SELECT actors.id, name FROM actors
+      ORDER BY name")
+  end
+end
+
+def movies_data_with_actor_id
+actor_id = params[:id]
+
+db_connection do |conn|
+  conn.exec("SELECT movies.id, movies.title, actors.name, cast_members.character FROM cast_members
+        JOIN movies ON cast_members.movie_id = movies.id
+        JOIN actors ON cast_members.actor_id = actors.id
+        WHERE cast_members.actor_id = #{actor_id}")
+  end
+end
+
+
+###########################################################################
+
+get '/actors' do
+  @actors = actors
   erb :'actors/index'
 end
 
 get '/actors/:id' do
-
+  @movies_data_with_actor_id = movies_data_with_actor_id
   erb :'actors/show', locals: { actor_id: params[:id] }
 end
 
 get '/movies' do
-
+  @movie_table = movies
   erb :'movies/index'
 end
 
 get '/movies/:id' do
+  @movie_data = movie_data
+  @cast_data = cast_data
 
   erb :'movies/show', locals: { movie_id: params[:id] }
 end
